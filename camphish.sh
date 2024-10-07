b36dce596140217281eb44d54728179ed7a45d58
ICHRY <148702702+ICHRY0@users.noreply.github.com>
2024-10-07 23:18:44 +0530

    Update camphish.sh

--- camphish.sh
+++ camphish.sh
@@ -15,8 +15,8 @@ printf "\e[1;92m | |      |  ___  || |(_)| |\e[0m\e[1;77m|  _____)|  ___  |   | 
 printf "\e[1;92m | |      | (   ) || |   | |\e[0m\e[1;77m| (      | (   ) |   | |         ) || (   ) |\e[0m\n"
 printf "\e[1;92m | (____/\| )   ( || )   ( |\e[0m\e[1;77m| )      | )   ( |___) (___/\____) || )   ( |\e[0m\n"
 printf "\e[1;92m (_______/|/     \||/     \|\e[0m\e[1;77m|/       |/     \|\_______/\_______)|/     \|\e[0m\n"
-printf " \e[1;93m CamPhish Ver 1.7 \e[0m \n"
-printf " \e[1;77m www.techchip.net | youtube.com/techchipnet \e[0m \n"
+printf " \e[1;93m ICHCAM Ver 1.0 \e[0m \n"
+printf " \e[1;77m  | youtube.com/ICHRY0 \e[0m \n"
 
 printf "\n"
 
